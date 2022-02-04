a2f77195736de9d31cb904fc526d28c9c7a23314
AsiaEUniversity <99003287+AsiaEUniversity@users.noreply.github.com>
2022-02-04 14:02:53 +0800

    Add files via upload

---
+++ admin/includes/footer.php
@@ -0,0 +1,10 @@
+<footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                   &copy; 2017 Asia E University | Future Learn Here 
+                </div>
+
+            </div>
+        </div>
+    </footer>
---
+++ admin/includes/header.php
@@ -0,0 +1,49 @@
+<?php
+error_reporting(0);
+?>
+
+
+    <!-- HEADER END-->
+    <div class="navbar navbar-inverse set-radius-zero">
+        <div class="container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                 <a class="navbar-brand" href="#" style="color:#fff; font-family: 'Shadows Into Light', cursive; font-size:30px;4px; line-height:24px; ">
+				 
+                        
+                   Asia E University 
+				                 
+				                <br>
+				  <center><p style="font-size:30px;4px; color: #fff; line-height:24px; font-family: 'Shadows Into Light', cursive"> Course</p></center>
+				               
+				   <b><center><p style="font-size:30px; color: black; font-family: 'Pacifico', cursive"> Registration</p></center></b> 
+                </a>
+
+            </div>
+
+            <div class="left-div">
+				<i class="fa fa-mortar-board" style="font-size:56px;color:black"></i>
+				<i class="fa fa-mortar-board" style="font-size:56px;color:black"></i>
+				<i class="fa fa-mortar-board" style="font-size:56px;color:black"></i>
+				<i class="fa fa-mortar-board" style="font-size:56px;color:black"></i>
+				<i class="fa fa-mortar-board" style="font-size:56px;color:black"></i>
+				<i class="fa fa-mortar-board" style="font-size:56px;color:black"></i>
+				<i class="fa fa-mortar-board" style="font-size:56px;color:black"></i>
+				<i class="fa fa-mortar-board" style="font-size:56px;color:black"></i>
+				<i class="fa fa-mortar-board" style="font-size:56px;color:black"></i>
+				
+            </div>
+			
+			 <center><img src="includes/universitylogo.png" alt="Asia E University" width="300";height="200"></center>
+			 
+			 <br>
+			 <br>
+			
+		   </div>
+        </div>
+    </div>
+ 
